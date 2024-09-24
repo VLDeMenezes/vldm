c46653e726dc3731fa0fc8684f274ae4f80cb88f
VLDeMenezes <estudiodemenezes@gmail.com>
2024-09-23 22:02:26 -0300

    CONTACT ICONS

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { contact } from "@/helper/contact";
 import { project } from "@/helper/project";
 import Image from "next/image";
 
@@ -16,10 +17,8 @@ export default function Home() {
           className="rounded-full p-2 my-auto"
         />
         <article className="flex flex-col gap-2 justify-center">
-          <p>
-            Hola! me presento:
-            <h1 className="text-3xl">VICTOR DE MENEZES</h1>
-          </p>
+          <p>Hola! me presento:</p>
+          <h1 className="text-3xl">VICTOR DE MENEZES</h1>
 
           <p>
             Este nombre es el que ayudará a tu empresa a lograr sus objetivos
@@ -30,7 +29,7 @@ export default function Home() {
         className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Jobs"
       >
-        <h2 className="text-2xl m-auto"> Mis trabajos</h2>
+        <h2 className="text-2xl m-auto"> MIS TRABAJOS</h2>
         <p className="m-auto italic">
           Actualmente me encuentro trabajando en mi propio Estudio Juridico
         </p>
@@ -39,7 +38,7 @@ export default function Home() {
         className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Projects"
       >
-        <h2 className="text-2xl m-auto"> Mis Proyectos</h2>
+        <h2 className="text-2xl m-auto"> MIS PROYECTOS</h2>
         <ul className="m-auto flex flex-wrap gap-5">
           {project.map((item) => (
             <a
@@ -66,7 +65,23 @@ export default function Home() {
         className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Contact"
       >
-        <h2 className="text-2xl m-auto">Contacto</h2>
+        <h2 className="text-2xl m-auto">CONTACTO</h2>
+        <p className="m-auto">
+          Podras contactarme a traves de cualquiera de los siguientes medios:
+        </p>
+        <article className="flex flex-wrap gap-2 justify-around m-auto">
+          {contact.map((item) => (
+            <a
+              target="_blank"
+              href={item.path}
+              key={item.name}
+              aria-label={item.name}
+              className="m-auto size-[10vw] flex  bg-SectionDark dark:bg-SectionLigth hover:scale-105 rounded-lg"
+            >
+              <p className="text-5xl m-auto">{item.icon}</p>
+            </a>
+          ))}
+        </article>
       </section>
     </main>
   );
---
+++ src/helper/contact.js
@@ -0,0 +1,18 @@
+import { IoLogoGithub, IoLogoLinkedin, IoMail } from "react-icons/io5";
+export const contact = [
+  {
+    name: "Mail",
+    path: "mail:estudiodemenezes@gmail.com",
+    icon: <IoMail />,
+  },
+  {
+    name: "Linkedin",
+    path: "https://www.linkedin.com/in/demenezesvictor/",
+    icon: <IoLogoLinkedin />,
+  },
+  {
+    name: "GitHub",
+    path: "https://github.com/VLDeMenezes",
+    icon: <IoLogoGithub />,
+  },
+];
