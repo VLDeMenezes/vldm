5916b8cbd31643c7a513824bf2a262693192ed6e
VLDeMenezes <estudiodemenezes@gmail.com>
2024-09-23 21:40:18 -0300

    Projects render

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { project } from "@/helper/project";
 import Image from "next/image";
 
 export default function Home() {
@@ -26,19 +27,43 @@ export default function Home() {
         </article>
       </section>
       <section
-        className="bg-SectionLigth dark:bg-SectionDark flex flex-row justify-around my-2 rounded-lg min-h-[80vh]"
+        className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Jobs"
       >
         <h2 className="text-2xl m-auto"> Mis trabajos</h2>
+        <p className="m-auto italic">
+          Actualmente me encuentro trabajando en mi propio Estudio Juridico
+        </p>
       </section>
       <section
-        className="bg-SectionLigth dark:bg-SectionDark flex flex-row justify-around my-2 rounded-lg min-h-[80vh]"
+        className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Projects"
       >
         <h2 className="text-2xl m-auto"> Mis Proyectos</h2>
+        <ul className="m-auto flex flex-wrap gap-5">
+          {project.map((item) => (
+            <a
+              className="flex flex-col p-2 shadow-lg text-center size-[20vw] bg-SectionDark dark:bg-SectionLigth"
+              href={item.path}
+              target="_blank"
+              aria-label={`Proyecto ${item.name}`}
+              key={item.name}
+            >
+              <h3 className="text-2xl">{item.name}</h3>
+              <Image
+                src={item.image}
+                alt="Imagen del proyecto"
+                width={200}
+                height={200}
+                className="m-auto"
+              />
+              <p className="text-wrap">{item.description}</p>
+            </a>
+          ))}
+        </ul>
       </section>
       <section
-        className="bg-SectionLigth dark:bg-SectionDark flex flex-row justify-around my-2 rounded-lg min-h-[80vh]"
+        className="bg-SectionLigth dark:bg-SectionDark flex flex-col justify-around my-2 rounded-lg min-h-[80vh]"
         id="Contact"
       >
         <h2 className="text-2xl m-auto">Contacto</h2>
---
+++ src/helper/project.js
@@ -0,0 +1,16 @@
+export const project = [
+  {
+    name: "NearVet",
+    image: "/NearVetcaptura.png",
+    path: "",
+    description:
+      " App para veterinarias con CRUD de Usuarios, Mascotas, Veterinarios, Turnos, Historias clinicas, con integración de pasarela de pagos ",
+  },
+  {
+    name: "ShopAll",
+    image: "/shopall.png",
+    path: "",
+    description:
+      "Modelo de E-comerce con autenticación para el pedido de ordenes de electronica",
+  },
+];
