b8f8000490e39802d3db02b0a48ef831f81d76e2
Victor <estudiodemenezes@gmail.com>
2024-10-06 16:39:19 -0300

    modal projects and styling and readme

--- src/app/ProjectSection.js
+++ src/app/ProjectSection.js
@@ -1,8 +1,17 @@
 import { project } from "@/helper/project";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
+import { IoCloseCircle, IoCloseCircleOutline } from "react-icons/io5";
 
 const ProjectSection = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const handleOpenModal = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
   return (
     <section
       className="bg-SectionLigth dark:bg-SectionDark flex flex-col my-2 p-3 justify-center rounded-lg min-h-[100vh] mt-[10vh]"
@@ -12,37 +21,124 @@ const ProjectSection = () => {
         {" "}
         My latest Projects
       </h2>
-      <ul className="mx-auto my-10 flex flex-wrap gap-10 justify-center">
+
+      <ul className="mx-auto my-10 flex flex-wrap gap-4 justify-center">
         {project.map((item) => (
-          <a
-            className="flex flex-col justify-around shadow-lg text-justify p-4 md:size-[25vw] bg-SectionDark dark:bg-SectionLigth hover:scale-105 duration-150 rounded-lg   cursor-pointer "
-            href={item.path}
-            target="_blank"
+          <div
+            className="group flex flex-col justify-between shadow-lg text-justify p-4 w-full sm:w-[90%] md:w-[45%] lg:w-[30%] bg-SectionDark dark:bg-SectionLight hover:scale-105 duration-300 ease-in-out transform rounded-lg cursor-pointer"
+            onClick={() => handleOpenModal(item)}
             aria-label={`Proyecto ${item.name}`}
             key={item.name}
           >
-            <h3 className="text-2xl lg:text-3xl text-center font-semibold text-SectionLigth ">
+            <h3 className="text-xl md:text-2xl lg:text-3xl text-center font-semibold text-SectionLight">
               {item.name}
             </h3>
+
             <Image
               src={item.image}
               alt={`Imagen del Proyecto ${item.name}`}
               width={200}
               height={200}
               layout="responsive"
-              className="mx-auto my-2 lg:size-[10vw]"
+              className="mx-auto my-2"
             />
-            <p className="text-wrap lg:text-lg">{item.description}</p>
+
+            <p className="text-sm md:text-base lg:text-lg">
+              {item.description}
+            </p>
+
             <small
-              className={`text-end lg:text-lg font-semibold ${
-                item.status === "Deployed" ? "text-green-600" : "text-red-600"
+              className={`text-end text-sm md:text-base lg:text-lg font-semibold ${
+                item.status === "Deployed"
+                  ? "text-green-600"
+                  : "text-red-600 cursor-default"
               }`}
             >
               {item.status}
             </small>
-          </a>
+
+            <div className="hidden group-hover:flex group-hover:flex-row gap-2 items-center text-3xl justify-center mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out ">
+              {item.tecs.map((tec) => (
+                <p key={tec.name} className="hover:scale-105 hover:text-Detail">
+                  {tec.icon}
+                </p>
+              ))}
+            </div>
+          </div>
         ))}
       </ul>
+      {selectedProject && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-evenly items-center z-50"
+          onClick={handleCloseModal}
+        >
+          <div
+            className="bg-slate-100 text-black dark:bg-slate-800 rounded-lg p-4 w-[80%] h-[80%] md:w-1/2 md:h-5/6 my-5 text-center relative border-b-8  border-SectionDark shadow-2xl"
+            onClick={(e) => e.stopPropagation()} // Evitar cerrar el modal al hacer click dentro de él
+          >
+            <span className="absolute top-0 left-0 bg-Detail p-6 rounded-br-full">
+              {""}
+            </span>
+            <button
+              className="absolute top-0 right-0 p-4 text-xl text-red-500 hover:scale-110 hover:text-red-600"
+              onClick={handleCloseModal}
+              aria-label="Close"
+            >
+              <IoCloseCircle />
+            </button>
+            <h3 className="text-3xl font-semibold mb-4">
+              {selectedProject.name}
+            </h3>
+            <Image
+              src={selectedProject.image}
+              alt={`Imagen del Proyecto ${selectedProject.name}`}
+              width={400}
+              height={400}
+              className="mx-auto"
+            />
+            <p className=" my-4">{selectedProject.description}</p>
+            <div className="flex flex-row justify-center gap-4 ">
+              <a
+                className="text-lg font-semibold hover:scale-105 hover:text-Detail"
+                href={selectedProject.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Github
+              </a>
+              |
+              {selectedProject.status === "Deployed" ? (
+                <a
+                  className="block text-lg font-semibold text-green-600 hover:scale-105"
+                  href={selectedProject.path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View: {selectedProject.status}
+                </a>
+              ) : (
+                <small
+                  className={`block text-lg font-semibold text-red-600 cursor-default"
+              }`}
+                >
+                  {selectedProject.status}
+                </small>
+              )}
+            </div>
+            <div className="flex justify-center gap-4 mt-6 text-2xl ">
+              {selectedProject.tecs.map((tec) => (
+                <div
+                  key={tec.name}
+                  className="flex flex-col gap-2 items-center text-center hover:text-Detail cursor-default"
+                >
+                  <p className="text-2xl">{tec.icon}</p>
+                  <small className="hidden md:block">{tec.name}</small>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- src/helper/project.js
+++ src/helper/project.js
@@ -1,33 +1,114 @@
+import {
+  IoLogoCss3,
+  IoLogoFigma,
+  IoLogoVercel,
+  IoLogoReact,
+  IoLogoNodejs,
+  IoLogoHtml5,
+  IoLogoJavascript,
+} from "react-icons/io5";
+import { TbBrandTypescript } from "react-icons/tb";
+import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
+import {
+  SiExpress,
+  SiRender,
+  SiAxios,
+  SiBootstrap,
+  SiRedux,
+  SiPostgresql,
+  SiMongodb,
+} from "react-icons/si";
 export const project = [
   {
     name: "NearVet",
     image: "/NearVetcaptura.png",
     path: "https://near-vet-front-git-main-teamhvets-projects.vercel.app/",
     description:
-      " App for veterinary clinics with CRUD of Users, Pets, Veterinarians, Appointments, Medical records, with payment gateway integration",
+      " NearVet is a web app for veterinary clinics with CRUD of Users, Pets, Veterinarians, Appointments, Medical records, with payment gateway integration, it has different views and is connected to a database deployed in OnRender.",
     status: "Deployed",
+    tecs: [
+      { name: "TypeScript", icon: <TbBrandTypescript /> },
+      { name: "NextJs", icon: <RiNextjsLine /> },
+      { name: "Vercel", icon: <IoLogoVercel /> },
+      { name: "TailwindCss", icon: <RiTailwindCssFill /> },
+      { name: "Express", icon: <SiExpress /> },
+      { name: "Render", icon: <SiRender /> },
+    ],
+    github: "https://github.com/VLDeMenezes/NearVet",
   },
   {
     name: "ShopAll",
     image: "/shopallCaptura.png",
     path: "https://github.com/VLDeMenezes/ShopAll",
     description:
-      "E-commerce model with authentication for the ordering of electronics items",
+      "ShopAll is an e-commerce template created for a store of products such as cell phones, tablets and computers, mainly from the Apple market. It offers detailed product rendering with descriptions and prices from the PostGresSql database.",
     status: "Not deployed",
+    tecs: [
+      { name: "NextJs", icon: <RiNextjsLine /> },
+      { name: "TailwindCss", icon: <RiTailwindCssFill /> },
+      { name: "Express", icon: <SiExpress /> },
+      { name: "Axios", icon: <SiAxios /> },
+      { name: "PostgreSQL", icon: <SiPostgresql /> },
+    ],
+    github: "https://github.com/VLDeMenezes/ShopAll",
   },
   {
     name: "Leyria & De Menezes",
     image: "/leyriaCaputara.png",
     path: "https://l-dm.vercel.app/",
-    description: "Law firm app to schedule appointments online",
+    description:
+      "Leyria & De Menezes is a web application for scheduling appointments with a law firm, applicable to law firms, it allows the publication of news and certifications as well as scheduling appointments online.",
     status: "Deployed",
+    tecs: [
+      { name: "HTML 5", icon: <IoLogoReact /> },
+      { name: "Axios", icon: <SiAxios /> },
+      { name: "Bootstrap", icon: <SiBootstrap /> },
+      { name: "ReduxJs", icon: <SiRedux /> },
+      { name: "Express", icon: <SiExpress /> },
+      { name: "PostgreSQL", icon: <SiPostgresql /> },
+    ],
+    github: "https://github.com/VLDeMenezes/L-DM",
   },
   {
     name: "ShowFilms",
     image: "/showfilmsCaputa.png",
     path: "https://github.com/VLDeMenezes/ShowFilms",
     description:
-      "App to be able to show movies, their description, authors, director and their rating through a backend",
+      "ShowFilms is a platform that displays descriptions, authors, directors and contextual information about different movies, allowing you to rate them. Created with basic technologies (HTML, CSS & JavaScript) to demonstrate the potential that can arise without the use of frameworks or other libraries. Uses a mongodb as DataBase",
     status: "Not deployed",
+    tecs: [
+      { name: "HTML 5", icon: <IoLogoHtml5 /> },
+      { name: "CSS 3", icon: <IoLogoCss3 /> },
+      { name: "JavaScript", icon: <IoLogoJavascript /> },
+      { name: "MongoDB", icon: <SiMongodb /> },
+    ],
+    github: "https://github.com/VLDeMenezes/ShowFilms",
+  },
+  {
+    name: "SumarioLegalApp",
+    image: "/sentenciasApp.png",
+    path: "https://sentenciasapp.vercel.app/",
+    description:
+      "Sumario Legal App is a web app that allows you to upload and summarize criminal records using AI Geminis and download the synopsis in editable word. Designed to automate summary tasks for court rulings and decisions",
+    status: "Deployed",
+    tecs: [
+      {
+        name: "TypeScript",
+        icon: <TbBrandTypescript />,
+      },
+      {
+        name: "NextJs",
+        icon: <RiNextjsLine />,
+      },
+      {
+        name: "Vercel",
+        icon: <IoLogoVercel />,
+      },
+      {
+        name: "TailwindCss",
+        icon: <RiTailwindCssFill />,
+      },
+    ],
+    github: "https://github.com/VLDeMenezes/sentenciasapp",
   },
 ];
