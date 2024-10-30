c63d3ca6aabec9ee97383bc4d0e319ed1fe6a02f
Victor <estudiodemenezes@gmail.com>
2024-10-30 12:13:54 -0300

    Security improves

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  connect-src 'self';
+  font-src 'self';
+`;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Aplica estas cabeceras a todas las rutas
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "no-referrer" },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=(), camera=()",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
