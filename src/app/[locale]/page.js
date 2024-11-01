041aef0dbd7b17882d1db1222e96759a44a6d4d8
Victor <estudiodemenezes@gmail.com>
2024-11-01 10:37:01 -0300

    move files to locale

---
+++ src/app/[locale]/page.js
@@ -0,0 +1,74 @@
+"use client";
+import JobSection from "./JobSection";
+import ContactSection from "./ContactSection";
+import ProjectSection from "./ProjectSection";
+import Link from "next/link";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import initTranslations from "../i18n";
+import TranslationsProvider from "../../components/TranslationsProvider";
+const i18nNamespaces = ["home"];
+
+export default async function Home({ params: { locale } }) {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+  return (
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}
+    >
+      <main className="mx-auto w-[90%] md:w-[75%]">
+        <section
+          className="bg-SectionLigth dark:bg-SectionDark flex flex-col my-2 p-3 justify-center rounded-lg min-h-[70vh] md:min-h-[100vh] md:mt-[13vh] relative"
+          id="Home"
+        >
+          {/* SVG */}
+          <div className=" hidden md:flex flex-col gap-4 items-center absolute top-5 right-5 group cursor-pointer">
+            <Link
+              href="/movile"
+              alt="probar app en vista de movil"
+              className="text-4xl bg-SectionDark dark:bg-SectionLigth rounded-full p-2"
+            >
+              <IoPhonePortraitOutline />
+            </Link>
+
+            {/* Tooltip (Texto flotante) */}
+            <p className="absolute top-[10vh] w-[10vw] text-center left-1/2 transform -translate-x-1/2 hidden md:group-hover:block text-sm bg-gray-800 text-white p-1 rounded-md duration-200">
+              Experimenta la versión móvil
+            </p>
+          </div>
+
+          <p className="ml-[5vw] mt-[5vh] text-xl lg:text-2xl dark:text-zinc-50">
+            {t("saludo")}
+          </p>
+          <div className="size-[90%] md:size-[80%] mt-2 rounded-lg overflow-hidden mx-auto">
+            <lottie-player
+              src="/profileAndName.json"
+              background="transparent"
+              speed="1"
+              autoplay
+            ></lottie-player>
+          </div>
+          <p className="text-center md:text-start md:ml-[8vw] lg:ml-[12vw] dark:text-zinc-50">
+            Full Stack Developer | Lawyer
+          </p>
+          <p className="text-wrap m-2 text-center text-SectionDark dark:text-SectionLigth font-semibold italic animate-pulse lg:text-xl drop-shadow-xl transition-transform duration-500 hover:scale-110">
+            This name will help you achieve your company`s goals.
+          </p>
+
+          <div className="bg-SectionDark dark:bg-Detail md:mx-[10vw] p-2 lg:p-4 lg:mt-8 rounded-lg">
+            As Full Stack developer and a lawyer, my career is marked by a deep
+            curiosity and a constant adaptation to change.My passion for
+            technology is reflected in my ability to automate processes. I
+            integrate diverse experiences into creative solutions that improve
+            the quality of life. I am committed to being at the forefront of
+            innovation.
+          </div>
+        </section>
+
+        <JobSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
+    </TranslationsProvider>
+  );
+}
