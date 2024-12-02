75e061cd61dff51b28ea64c55820f682e1a845f7
VLDeMenezes <estudiodemenezes@gmail.com>
2024-12-02 10:55:09 -0300

    New Job

--- src/helper/Jobs.js
+++ src/helper/Jobs.js
@@ -3,6 +3,12 @@ import { FaComputer } from "react-icons/fa6";
 import { TbDeviceIpadMinus } from "react-icons/tb";
 export const Jobs = [
   {
+    name: "Happ3n Front End",
+    description:
+      "Developing the new version of happ3n, a web app that brings together people interested in web3 and events taking place in different countries, allowing them to interact also on their own channels and hubs",
+    icon: <FaComputer />,
+  },
+  {
     name: "Web Developer",
     description:
       "Since 2 years ago, I started exploring the world of web development, discovering a new passion for technology and FrontEnd development in particular, but also Full Stack. My self-taught approach has allowed me to acquire solid knowledge in HTML, CSS, JavaScript, and frameworks such as React and Next.js. I am excited to combine my legal background with my new technical skills to offer effective solutions in the digital field.",
