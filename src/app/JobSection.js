24f8047c91725220fd92f68fc5a661c783cc01a6
VLDeMenezes <estudiodemenezes@gmail.com>
2025-01-03 08:15:22 -0300

    Update job section with new roles and descriptions; adjust layout for better alignment

--- src/app/JobSection.js
+++ src/app/JobSection.js
@@ -15,7 +15,7 @@ const JobSection = () => {
         {Jobs.map((job) => (
           <div
             key={job.name}
-            className="flex flex-col justify-center shadow-lg text-justify p-4 md:size-[25vw] bg-SectionDark dark:bg-SectionLigth md:hover:scale-105 duration-150 rounded-lg  cursor-default relative"
+            className="flex flex-col justify-start shadow-lg text-justify p-4 max-w-[350px] bg-SectionDark dark:bg-SectionLigth md:hover:scale-105 duration-150 rounded-lg  cursor-default relative"
           >
             <p
               className="md:text-5xl absolute top-0 md:top-[-6vh] z-10 md:rigth-[49%] lg:top-[-4vh] bg-SectionDark dark:bg-SectionLigth p-3 rounded-full"
@@ -24,7 +24,7 @@ const JobSection = () => {
               {job.icon}
             </p>
 
-            <strong className="text-center lg:text-xl">{job.name}</strong>
+            <strong className="text-center lg:text-xl mt-5">{job.name}</strong>
             <p className="text-wrap overflow-hidden lg:text-xl">
               {job.description}
             </p>
--- src/helper/Jobs.js
+++ src/helper/Jobs.js
@@ -3,9 +3,21 @@ import { FaComputer } from "react-icons/fa6";
 import { TbDeviceIpadMinus } from "react-icons/tb";
 export const Jobs = [
   {
-    name: "Web Developer",
+    name: "Happ3n Front End",
     description:
-      "Since 2 years ago, I started exploring the world of web development, discovering a new passion for technology and FrontEnd development in particular, but also Full Stack. My self-taught approach has allowed me to acquire solid knowledge in HTML, CSS, JavaScript, and frameworks such as React and Next.js. I am excited to combine my legal background with my new technical skills to offer effective solutions in the digital field.",
+      "Developing the new version of happ3n, a web app that brings together people interested in web3 and events taking place in different countries, allowing them to interact also on their own channels and hubs",
+    icon: <FaComputer />,
+  },
+  {
+    name: "Prisma Tec Front End",
+    description:
+      "Making profesional web pages for clients around the world, using React, Next.js, and TailwindCSS, and also developing the company's website. ",
+    icon: <FaComputer />,
+  },
+  {
+    name: "Freelancer Web Developer",
+    description:
+      "Since 4 years ago, I started exploring the world of web development, discovering a new passion for technology and FrontEnd development in particular, but also Full Stack. My self-taught approach has allowed me to acquire solid knowledge in HTML, CSS, JavaScript, and frameworks such as React and Next.js. I am excited to combine my legal background with my new technical skills to offer effective solutions in the digital field.",
     icon: <FaComputer />,
   },
   {
@@ -13,11 +25,5 @@ export const Jobs = [
     description:
       "Over 5 years of experience in legal practice and a postgraduate degree in procedural law. Throughout my career, I have developed analytical skills and attention to detail that allow me to address complex problems with innovative solutions.",
     icon: <GoLaw />,
-  },
-  {
-    name: "Administrative",
-    description:
-      "More than 2 years of experience in the management of administrative records, public interaction, management of state projects and development in human resources management.",
-    icon: <TbDeviceIpadMinus />,
   },
 ];
